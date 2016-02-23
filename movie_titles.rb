f5f0de87f96b096bbaa3b967a23b28cc48926e98
codeByScott <scott@codebyscott.io>
2016-02-24 00:23:57 +0900

    completed movie_titles.rb

---
+++ movie_titles.rb
@@ -0,0 +1,28 @@
+# Write a program that uses a hash to store a list of movie titles with the year they came out. 
+# Then use the puts command to make your program print out the year of each movie to the screen. 
+# The output for your program should look something like this.
+# 1975
+# 2004
+# 2013
+# 2001
+# 1981
+
+ ################################################
+#                                                # 
+# ### ### ###  ###  ### # #  ### ### ### ### ### #
+# #   # # #  # #    # #  #   #   #   # #  #   #  #
+# #   # # #  # ###  ##   #   ### #   # #  #   #  #
+# #   # # #  # #    # #  #     # #   # #  #   #  #
+# ### ### ###  ###  ###  #   ### ### ###  #   #  #
+#                                                #
+#                   O   O                        #
+#                     *                          #
+# <3              "  \_/  "                   <3 #
+ ################################################
+
+action_movies = { "Rambo": 1982, 
+                  "Terminator": 1985, 
+                  "Die Hard": 1989,
+                  "Speed": 1994}
+
+puts action_movies.values
