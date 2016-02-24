1f7e30d5e318203f58ec030c961a261361027b55
codeByScott <scott@codebyscott.io>
2016-02-24 15:16:07 +0900

    completed exercise

---
+++ movie_titles_array.rb
@@ -0,0 +1,16 @@
+# Use the dates from the previous example and store them in an array. 
+# Then make your program output the same thing as exercise 3.
+action_movies = 
+[
+  ["rambo", 1982], 
+  ["terminator", 1985], 
+  ["die_hard", 1989], 
+  ["speed", 1994]
+]
+
+puts action_movies[0][1]
+puts action_movies[1][1]
+puts action_movies[2][1]
+puts action_movies[3][1]
+
+# After verifying with the solution, I realized that they really just wanted an array of the dates, but this seemed cooler.
