3fa6aa42b4127725a55fa0fbdc22bdd109a76f0c
codeByScott <scott@codebyscott.io>
2016-02-24 16:28:56 +0900

    exercise complete

---
+++ methods/greeting.rb
@@ -0,0 +1,12 @@
+# Write a program that prints a greeting message.
+# This program should contain a method called greeting 
+# that takes a name as its parameter and returns a string.
+
+def greeting(name)
+  "Hello, #{name.capitalize}!"
+end
+
+puts "What is your name?"
+name = gets.chomp.downcase
+
+puts greeting(name)
