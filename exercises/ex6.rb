f24fbe7727722745f35c5517f9aa4eae6b2f5ed7
codeByScott <scott@codebyscott.io>
2016-02-26 12:09:06 +0900

    exercise complete

---
+++ exercises/ex6.rb
@@ -0,0 +1,9 @@
+# Get rid of duplicates without specifically 
+# removing any one value.
+
+duplicates = [1, 2, 3, 3, 4, 5, 6, 7, 7, 8, 9, 10]
+
+no_duplicates = duplicates.uniq
+
+p duplicates
+p no_duplicates
---
+++ exercises/ex8.rb
@@ -0,0 +1,10 @@
+# Create a Hash using both Ruby syntax styles.
+
+hash1 = Hash.new
+hash2 = {:key => 'value'}
+hash3 = {key: 'value'}
+
+p hash1
+p hash2
+p hash3
+
