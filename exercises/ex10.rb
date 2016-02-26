268cc43583c80f8c49ddb6d7efc4b3f00ed44477
codeByScott <scott@codebyscott.io>
2016-02-26 12:39:04 +0900

    exercise complete

---
+++ exercises/ex10.rb
@@ -0,0 +1,6 @@
+# hash values as arrays
+my_siblings = { brothers: ['ryan', 'bobby', 'eric'], sisters: ['kristi', 'leeanna'] }
+
+# array of hashes
+brothers_names = [{name: 'ryan'}, {name: 'eric'}, {name: 'bobby'}]
+
