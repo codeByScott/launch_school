98a531118af41f95aac14d575d5bfe5afe054790
codeByScott <scott@codebyscott.io>
2016-03-04 23:55:48 +0900

    exercise complete

--- programming-backend-dev/programming-foundations/lesson_2/loan-calc.rb
+++ programming-backend-dev/programming-foundations/lesson_2/loan-calc.rb
@@ -3,47 +3,60 @@
 # - Get the loan amount
 # - Get the annual percentage rate
 # - get the loan duration
-# display monthly loan amount
-# display total loan amount after interest
+# - display monthly loan amount
 
-
-loan_amt = ''
-loop do
-  print "Enter loan amount: "
-  loan_amt = Float(gets) rescue false
-  if loan_amt
-    break
-  else
-    puts "Please enter a proper loan amount (numbers only)"
-  end
+def prompt(message)
+  print "=> #{message}"
 end
 
-monthly_rate = ''
-loop do
-  print "Enter Annual Percentage Rate (APR) (i.e. 7.25): "
-  annual_rate = Float(gets) rescue false
-  if annual_rate and annual_rate.to_i > 0
-    monthly_rate = (annual_rate / 100) / 12
-    break
-  else
-    puts "Oops...that doesn't seem to be valid!"
+puts 
+puts "-" * 50
+puts "MORTGAGE CALCULATOR".center(50)
+puts "-" * 50
+
+
+loop do # main loop
+  puts
+  loan_amt = ''
+  loop do
+    prompt "Enter loan amount: "
+    loan_amt = Float(gets) rescue false
+    if loan_amt and loan_amt.to_i > 0
+      break
+    else
+      prompt "Oops...make sure it is a number greater than 0.\n"
+    end
   end
-end
 
-loan_length = ''
-loop do
-  print "Enter loan duration (in months): "
-  loan_length = Integer(gets) rescue false
-  if loan_length and loan_length > 0
-    break
-  else
-    puts "Oops...that doesn't seem to be valid!"
+  monthly_rate = ''
+  loop do
+    prompt "Enter Annual Percentage Rate (APR) (i.e. 7.25): "
+    annual_rate = Float(gets) rescue false
+    if annual_rate and annual_rate.to_i > 0
+      monthly_rate = (annual_rate / 100) / 12
+      break
+    else
+      prompt "Oops...that doesn't seem to be valid!\n"
+    end
   end
+
+  loan_length = ''
+  loop do
+    prompt "Enter loan duration (in months): "
+    loan_length = Integer(gets) rescue false
+    if loan_length and loan_length > 0
+      break
+    else
+      prompt "Oops...that doesn't seem to be valid!\n"
+    end
+  end
+
+  monthly_amount = (loan_amt * (1 + monthly_rate)**loan_length * monthly_rate) / ((1 + monthly_rate)**loan_length - 1)
+
+  prompt "Your monthly payment is: $#{format('%02.2f', monthly_amount)}\n"
+  prompt "Would you like to make another calculation? ('Y' or 'N'): "
+  again = gets.chomp
+  break unless again.downcase.start_with?('y')
 end
-
-monthly_amount = (loan_amt * (1 + monthly_rate)**loan_length * monthly_rate) / ((1 + monthly_rate)**loan_length - 1)
-
-puts "Your monthly payment is: #{monthly_amount.round(2)}"
-
 
 
