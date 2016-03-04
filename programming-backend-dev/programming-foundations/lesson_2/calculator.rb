894850ddaaf32e5d5b793bc7217904b925d79e04
codeByScott <scott@codebyscott.io>
2016-03-04 14:30:13 +0900

    exercise complete

--- programming-backend-dev/programming-foundations/lesson_2/calculator.rb
+++ programming-backend-dev/programming-foundations/lesson_2/calculator.rb
@@ -6,29 +6,107 @@
 # output the result
 
 # answer = Kernel.gets()
-# Kernel.puts(answer) 
-
-Kernel.puts("Welcome to Calculator!")
-
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp()
-Kernel.puts("The number is " + number1 + "!")
-
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp()
-Kernel.puts("The number is: " + number2 + "!")
-
-Kernel.puts("What operation would you like to perform? \n1) add \n2) subtract \n3) multiply \n4) divide")
-operator = Kernel.gets().chomp()
-
-if operator == "1"
-  result = number1.to_i() + number2.to_i()
-elsif operator == "2"
-  result = number1.to_i() - number2.to_i()
-elsif operator == "3"
-  result = number1.to_i() * number2.to_i()
-else
-  result = number1.to_f() / number2.to_f()
+# Kernel.puts(answer)
+def prompt(msg)
+  Kernel.puts("=> #{msg}")
 end
 
-Kernel.puts("The result is: #{result.to_f().round(2)}")
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operating_to_msg(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt("Welcome to Calculator!  Enter your name:")
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+  
+  if name.empty?()
+    prompt("Make sure to use a valid name.")
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}")
+
+loop do # main loop
+  number1 = ''
+  loop do
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm...that doesn't look like a valid number.")
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm...that doesn't look like a valid number.")
+    end
+  end
+
+  operator_prompt = <<-MSG
+  What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+MSG
+  
+  prompt(operator_prompt)
+  
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+    
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3 or 4")
+    end
+  end
+  
+  prompt("#{operating_to_msg(operator)} the two numbers...")
+  sleep 1
+  
+  result = case operator
+    when '1'
+      result = number1.to_i() + number2.to_i()
+    when '2'
+      result = number1.to_i() - number2.to_i()
+    when '3'
+      result = number1.to_i() * number2.to_i()
+    when '4'
+      result = number1.to_f() / number2.to_f()
+  end
+
+  prompt("The result is: #{result.to_f().round(2)}")
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Good Bye!")
