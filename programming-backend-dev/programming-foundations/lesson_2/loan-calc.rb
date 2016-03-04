249afc323b9a8682b80698e4324aa6e32bf71960
codeByScott <scott@codebyscott.io>
2016-03-04 23:27:03 +0900

    added error rescues

--- programming-backend-dev/programming-foundations/lesson_2/loan-calc.rb
+++ programming-backend-dev/programming-foundations/lesson_2/loan-calc.rb
@@ -6,15 +6,40 @@
 # display monthly loan amount
 # display total loan amount after interest
 
-print "Enter loan amount: "
-loan_amt = gets.chomp.to_f
 
-print "Enter Annual Percentage Rate (APR) (i.e. 7.25): "
-annual_rate = gets.chomp.to_f
-monthly_rate = (annual_rate / 100) / 12
+loan_amt = ''
+loop do
+  print "Enter loan amount: "
+  loan_amt = Float(gets) rescue false
+  if loan_amt
+    break
+  else
+    puts "Please enter a proper loan amount (numbers only)"
+  end
+end
 
-print "Enter the loan duration (in months): "
-loan_length = gets.chomp.to_f
+monthly_rate = ''
+loop do
+  print "Enter Annual Percentage Rate (APR) (i.e. 7.25): "
+  annual_rate = Float(gets) rescue false
+  if annual_rate and annual_rate.to_i > 0
+    monthly_rate = (annual_rate / 100) / 12
+    break
+  else
+    puts "Oops...that doesn't seem to be valid!"
+  end
+end
+
+loan_length = ''
+loop do
+  print "Enter loan duration (in months): "
+  loan_length = Integer(gets) rescue false
+  if loan_length and loan_length > 0
+    break
+  else
+    puts "Oops...that doesn't seem to be valid!"
+  end
+end
 
 monthly_amount = (loan_amt * (1 + monthly_rate)**loan_length * monthly_rate) / ((1 + monthly_rate)**loan_length - 1)
 
