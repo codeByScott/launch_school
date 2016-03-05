22c13e64aa5183c06f4ccf4d035396eb003beeb5
codeByScott <scott@codebyscott.io>
2016-03-05 23:43:18 +0900

    initial draft

---
+++ programming-backend-dev/programming-foundations/lesson_2/rpssl.rb
@@ -0,0 +1,66 @@
+# keep score
+# whoever reaches five points first wins
+
+VALID_CHOICES = %w(rock paper scissors spock lizard).freeze
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def win_round?(first, second)
+  first == 'rock'     && second == 'scissors' ||
+  first == 'rock'     && second == 'lizard'   ||
+  first == 'paper'    && second == 'rock'     ||
+  first == 'paper'    && second == 'spock'    ||
+  first == 'scissors' && second == 'paper'    ||
+  first == 'scissors' && second == 'lizard'   ||
+  first == 'spock'    && second == 'scissors' ||
+  first == 'spock'    && second == 'rock'     ||
+  first == 'lizard'   && second == 'spock'    ||
+  first == 'lizard'   && second == 'paper'
+end
+
+def display_results(player, computer)
+  if win_round?(player, computer)
+    "You won!"
+  elsif win_round?(computer, player)
+    "Computer won!"
+  else
+    "It's a tie!"
+  end
+end
+
+wins = losses = draws = 0
+def score_keeper(player, computer)
+  if win_round?(player, computer)
+    wins += 1
+  elsif win_round?(computer, player)
+    losses += 1
+  else
+    draws += 1
+  end
+end
+
+puts
+puts "-" * 75
+puts "ROCK, PAPER, SCISSORS, SPOCK, LIZARD".ljust(50) + "Created by CodeByScott".rjust(25)
+puts "-" * 75
+
+3.times do # main loop
+    
+  choice = ''
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice = gets.chomp
+    break if VALID_CHOICES.include?(choice)
+    prompt "That's not a valid choice!"
+  end
+
+  computer_choice = VALID_CHOICES.sample
+  prompt "You chose: #{choice}, the computer chose: #{computer_choice}."
+  prompt display_results(choice, computer_choice)
+  prompt "Wins: #{wins} Losses: #{losses} Draws: #{draws}"
+
+end
+
+prompt "Thank you for playing RPSSL!"
