92025be3e8242acf9c2af45f7ff2d59a3a85aae4
codeByScott <scott@codebyscott.io>
2016-03-07 09:55:23 +0900

    Exercise complete

---
+++ programming-backend-dev/programming-foundations/lesson_3/exercises-easy-3/ex03.rb
@@ -0,0 +1,19 @@
+# Question 3
+
+# In the previous exercise we added Dino to our array like this:
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+# flintstones << "Dino"
+
+# We could have used either Array#concat or 
+# Array#push to add Dino to the family.
+
+# How can we add multiple items to our array? (Dino and Hoppy)
+
+flintstones.push "Dino", "Hoppy"
+
+p flintstones
+
+# Launch School Solutions
+flintstones.push("Dino").push("Hoppy")
+flinstones.concat(%w(Dino Hoppy))
+
