8579b8c72b9b791f114c051d37020a0801426ce9
codeByScott <scott@codebyscott.io>
2016-03-08 21:20:57 +0900

    exercise complete

---
+++ programming-backend-dev/programming-foundations/lesson_3/exercises-medium-3/ex05.rb
@@ -0,0 +1,16 @@
+# Question 5
+
+# make better
+=begin
+def color_valid(color)
+  if color == "blue" || color == "green"
+    true
+  else
+    false
+  end
+end
+=end
+
+def color_valid(color)
+  color == "blue" || color == "green"
+end
