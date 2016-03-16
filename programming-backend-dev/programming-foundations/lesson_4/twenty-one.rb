ff2050cd8e53bc27961a4fd0ef0c43c3437fcedd
codeByScott <scott@codebyscott.io>
2016-03-16 12:05:31 +0900

    finished #total_of - counts ACE appropriately now

--- programming-backend-dev/programming-foundations/lesson_4/twenty-one.rb
+++ programming-backend-dev/programming-foundations/lesson_4/twenty-one.rb
@@ -10,16 +10,10 @@
 7. Compare cards and declare winner.
 =end
 
-# Cannot decide which to use yet
-SUITS_AND_VALUES = { h:{"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": [1, 11]},
-                     s:{"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": [1, 11]}, 
-                     d:{"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": [1, 11]},
-                     c:{"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": [1, 11]}}
-
-deck_of_cards = [ ['2', 'h'], ['3', 'h'], ['4', 'h'], ['5', 'h'], ['6', 'h'], ['7', 'h'], ['8', 'h'], ['9', 'h'], ['10', 'h'], ['j', 'h'], ['q', 'h'], ['k', 'h'], ['a', 'h'],
-                  ['2', 's'], ['3', 's'], ['4', 's'], ['5', 's'], ['6', 's'], ['7', 's'], ['8', 's'], ['9', 's'], ['10', 's'], ['j', 's'], ['q', 's'], ['k', 's'], ['a', 's'],
-                  ['2', 'd'], ['3', 'd'], ['4', 'd'], ['5', 'd'], ['6', 'd'], ['7', 'd'], ['8', 'd'], ['9', 'd'], ['10', 'd'], ['j', 'd'], ['q', 'd'], ['k', 'd'], ['a', 'd'],
-                  ['2', 'c'], ['3', 'c'], ['4', 'c'], ['5', 'c'], ['6', 'c'], ['7', 'c'], ['8', 'c'], ['9', 'c'], ['10', 'c'], ['j', 'c'], ['q', 'c'], ['k', 'c'], ['a', 'c']]
+deck = [['2', 'h'], ['3', 'h'], ['4', 'h'], ['5', 'h'], ['6', 'h'], ['7', 'h'], ['8', 'h'], ['9', 'h'], ['10', 'h'], ['j', 'h'], ['q', 'h'], ['k', 'h'], ['a', 'h'],
+        ['2', 's'], ['3', 's'], ['4', 's'], ['5', 's'], ['6', 's'], ['7', 's'], ['8', 's'], ['9', 's'], ['10', 's'], ['j', 's'], ['q', 's'], ['k', 's'], ['a', 's'],
+        ['2', 'd'], ['3', 'd'], ['4', 'd'], ['5', 'd'], ['6', 'd'], ['7', 'd'], ['8', 'd'], ['9', 'd'], ['10', 'd'], ['j', 'd'], ['q', 'd'], ['k', 'd'], ['a', 'd'],
+        ['2', 'c'], ['3', 'c'], ['4', 'c'], ['5', 'c'], ['6', 'c'], ['7', 'c'], ['8', 'c'], ['9', 'c'], ['10', 'c'], ['j', 'c'], ['q', 'c'], ['k', 'c'], ['a', 'c']]
 
 def shuffle!(deck)
   deck.shuffle!
@@ -57,30 +51,49 @@ def players_turn!(deck, players_hand, dealers_hand)
     display_total(players_hand)
     puts "Hit or Stay?:"
     choice = gets.strip
-    choice == 'hit' ? deal_card(deck, players_hand) : break
+    choice == 'h' ? deal_card(deck, players_hand) : break
     show_cards(players_hand, dealers_hand)
-    break if bust?(players_hand)
+    break if bust?(players_hand) || twenty_one?(players_hand)
   end
   show_cards(players_hand, dealers_hand)
-end  
+  display_total(players_hand)
+end
 
 def total_of(hand)
-  hand_total = 0
-  hand.each do |card|
-    hand_total += card_value(card[0], hand)
+  values = hand.map { |card| card[0] }
+  
+  sum = 0
+  values.each do |value|
+    if value == "a"
+      sum += 11
+    elsif value.to_i == 0 # J, Q, K
+      sum += 10
+    else
+      sum += value.to_i
+    end
   end
-  hand_total
+  # correcting for Aces  
+  values.select { |value| value == "a" }.count.times do
+    sum -= 10 if sum > 21
+  end
+  sum
 end
 
 def display_total(hand)
-  puts "Hand total: #{total_of(hand)}"
+  if twenty_one?(hand)
+    puts "Twenty One!"
+  elsif bust?(hand)
+    puts "Hand total: BUST!"
+  else
+    puts "Hand total: #{total_of(hand)}"
+  end
 end
 
 def initial_deal(deck, player, dealer)
-  # This is done so the player gets the 1st card from the top of the deck, 
+  # This is done so the player gets the 1st card from the top of the deck,
   # the dealer gets the 2nd, and so on.  Keeping it real!
-  2.times do 
-    deal_card(deck, player) 
+  2.times do
+    deal_card(deck, player)
     deal_card(deck, dealer)
   end
 end
@@ -89,26 +102,14 @@ def bust?(hand)
   total_of(hand) > 21
 end
 
-def card_value(card, hand)
-  if card == 'j' || card == 'q' || card == 'k' 
-    10
-  elsif card == 'a'
-    ace_value?(hand)
-  else card.to_i
-  end
-end
-
-def ace_value?(hand)
-  total_of(hand) > 10 ? 1 : 11
+def twenty_one?(hand)
+  total_of(hand) == 21
 end
 
 # GAME PLAY
-deck = deck_of_cards
 shuffle!(deck)
 initial_deal(deck, players_hand, dealers_hand)
 show_cards(players_hand, dealers_hand)
 players_turn!(deck, players_hand, dealers_hand)
 # dealer_plays
 # announce_winner
-
-
