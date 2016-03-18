88bce8fa7b098a13505ece37fc023f47d13e24b6
codeByScott <scott@codebyscott.io>
2016-03-18 12:28:50 +0900

    display cards horizontally

--- programming-backend-dev/programming-foundations/lesson_4/twenty-one.rb
+++ programming-backend-dev/programming-foundations/lesson_4/twenty-one.rb
@@ -1,61 +1,86 @@
-=begin
-1. Initialize deck
-2. Deal cards to player and dealer
-3. Player turn: hit or stay
-  - repeat until bust or "stay"
-4. If player bust, dealer wins.
-5. Dealer turn: hit or stay
-  - repeat until total >= 17
-6. If dealer bust, player wins.
-7. Compare cards and declare winner.
-=end
+# twenty-one.rb
+
+# * identified bugs *
+# shows dealers cards before the player is finished (need a flag)
+# does not end game when player gets Twenty One (Black Jack)
+# need to use 0 for 10 so the cards do not get skewed
+
 SUITS = %w(c d h s).freeze
-RANKS = %w(2 3 4 5 6 7 8 9 10 j q k a).freeze
+RANKS = %w(2 3 4 5 6 7 8 9 0 j q k a).freeze
 DECK = RANKS.product(SUITS)
 
 def deal_card(deck, hand)
   hand << deck.pop
 end
 
-def show_initial_cards(players_hand, dealers_hand)
-  puts "Dealer shows:"
-  puts ".---."
-  puts "|#{dealers_hand[0][0]}  |"
-  puts "| #{dealers_hand[0][1]} |"
-  puts "|  #{dealers_hand[0][0]}|"
-  puts ".---."
-  puts
-  puts "Players cards:"
-  players_hand.each do |card|
-    puts ".---. "
-    puts "|#{card[0]}  | "
-    puts "| #{card[1]} | "
-    puts "|  #{card[0]}| "
-    puts ".---. "
-    puts
+def card_line_border_for_hand(hand)
+  line = ""
+
+  hand.each do
+    line << " " unless line == "" # add a separator
+    line << ".---." # top/bottom border design
   end
+
+  line
 end
 
-# Had to separate initial_cards from show_cards, because I couldn't think of an easy way to only show the first dealer card.
+def card_line_one_for_hand(hand)
+  line = ""
+
+  hand.each do |card|
+    line << " " unless line == "" # add a separator
+    line << "|#{card[0]}  |" # card line design
+  end
+
+  line
+end
+
+def card_line_two_for_hand(hand)
+  line = ""
+
+  hand.each do |card|
+    line << " " unless line == "" # add a separator
+    line << "| #{card[1]} |" # card line design
+  end
+
+  line
+end
+
+def card_line_three_for_hand(hand)
+  line = ""
+
+  hand.each do |card|
+    line << " " unless line == "" # add a separator
+    line << "|  #{card[0]}|" # card line design
+  end
+
+  line
+end
+
+def print_hand(hand)
+  puts
+  puts card_line_border_for_hand(hand)
+  puts card_line_one_for_hand(hand)
+  puts card_line_two_for_hand(hand)
+  puts card_line_three_for_hand(hand)
+  puts card_line_border_for_hand(hand)
+  puts
+end
+
 def show_cards(players_hand, dealers_hand)
-  puts "Dealers cards:"
-  dealers_hand.each do |card|
-    puts ".---. "
-    puts "|#{card[0]}  | "
-    puts "| #{card[1]} | "
-    puts "|  #{card[0]}| "
-    puts ".---. "
-    puts
+  puts
+  puts "Dealer shows:"
+  if # player has finished
+    print_hand(dealers_hand)
+  else
+    puts ".---."
+    puts "|#{dealers_hand[0][0]}  |"
+    puts "| #{dealers_hand[0][1]} |"
+    puts "|  #{dealers_hand[0][0]}|"
+    puts ".---."
   end
   puts "Players cards:"
-  players_hand.each do |card|
-    puts ".---. "
-    puts "|#{card[0]}  | "
-    puts "| #{card[1]} | "
-    puts "|  #{card[0]}| "
-    puts ".---. "
-    puts
-  end
+  print_hand(players_hand)
 end
 
 def players_turn!(deck, players_hand, dealers_hand)
@@ -63,13 +88,12 @@ def players_turn!(deck, players_hand, dealers_hand)
   loop do
     display_total(players_hand)
     puts "Hit or Stay? ('h' or 's'):"
-      choice = gets.strip
-      choice == 'h' ? deal_card(deck, players_hand) : break
-      show_initial_cards(players_hand, dealers_hand)
+    choice = gets.strip
+    choice == 'h' ? deal_card(deck, players_hand) : break
+    show_cards(players_hand, dealers_hand)
     break if bust?(players_hand) || twenty_one?(players_hand)
   end
-  show_initial_cards(players_hand, dealers_hand)
-  display_total(players_hand)
+  choice
 end
 
 def dealers_turn!(deck, players_hand, dealers_hand)
@@ -85,22 +109,22 @@ end
 def total_of(hand)
   values = hand.map { |card| card[0] }
   sum = 0
-  
+
   values.each do |value|
     if value == "a"
       sum += 11
-    elsif value.to_i == 0 # J, Q, K
+    elsif value.to_i == 0 # 10, J, Q, K
       sum += 10
     else
       sum += value.to_i
     end
   end
-  
+
   # correcting for Aces
   values.select { |value| value == "a" }.count.times do
     sum -= 10 if sum > 21
   end
-  
+
   sum
 end
 
@@ -136,6 +160,8 @@ def announce_winner(player, dealer)
     puts "House Wins"
   elsif twenty_one?(player)
     puts "You Win!"
+  elsif total_of(player) == total_of(dealer)
+    puts "Push"
   else
     puts "You Win!"
   end
@@ -147,7 +173,7 @@ dealers_hand = []
 deck = DECK
 deck.shuffle!
 initial_deal(deck, players_hand, dealers_hand)
-show_initial_cards(players_hand, dealers_hand)
+show_cards(players_hand, dealers_hand)
 players_turn!(deck, players_hand, dealers_hand)
 dealers_turn!(deck, players_hand, dealers_hand)
 announce_winner(players_hand, dealers_hand)
