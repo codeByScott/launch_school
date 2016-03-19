7148f808de18b0019b8f35f44f6e598f18ed6683
codeByScott <scott@codebyscott.io>
2016-03-19 22:53:27 +0900

    fixed card display

--- programming-backend-dev/programming-foundations/lesson_4/twenty-one.rb
+++ programming-backend-dev/programming-foundations/lesson_4/twenty-one.rb
@@ -1,5 +1,5 @@
 # twenty-one.rb
-
+require 'pry'
 # * identified bugs *
 # shows dealers cards before the player is finished (need a flag)
 # does not end game when player gets Twenty One (Black Jack)
@@ -58,52 +58,80 @@ def card_line_three_for_hand(hand)
 end
 
 def print_hand(hand)
-  puts
   puts card_line_border_for_hand(hand)
   puts card_line_one_for_hand(hand)
   puts card_line_two_for_hand(hand)
   puts card_line_three_for_hand(hand)
   puts card_line_border_for_hand(hand)
-  puts
 end
 
-def show_cards(players_hand, dealers_hand)
-  puts
-  puts "Dealer shows:"
-  if # player has finished
-    print_hand(dealers_hand)
-  else
-    puts ".---."
-    puts "|#{dealers_hand[0][0]}  |"
-    puts "| #{dealers_hand[0][1]} |"
-    puts "|  #{dealers_hand[0][0]}|"
-    puts ".---."
+def print_first_card(hand)
+  puts ".---."
+  puts "|#{hand[0][0]}  |"
+  puts "| #{hand[0][1]} |"
+  puts "|  #{hand[0][0]}|"
+  puts ".---."
+end
+
+def show_players_cards(hand)
+  puts "\nPlayer:"
+  print_hand(hand)
+end
+
+def show_one_card(dealer)
+  puts "\nDealer:"
+  print_first_card(dealer)
+end
+
+def show_all_cards(dealer)
+  puts "\nDealers:"
+  print_hand(dealer)
+end
+
+def hit_or_stay?
+  choice = ''
+  loop do
+    puts "Hit or Stay?"
+    choice = gets.strip.downcase
+    break if choice == 'hit' || choice == 'stay'
+    puts "Please choose Hit or Stay!"
   end
-  puts "Players cards:"
-  print_hand(players_hand)
+  choice
+end
+
+def player_finished?(hand)
+  bust?(hand) || twenty_one?(hand) || total_of(hand) == 21
 end
 
 def players_turn!(deck, players_hand, dealers_hand)
-  choice = nil
   loop do
+    
+    system 'clear' or system 'cls'
+    show_one_card(dealers_hand)
+    
+    show_players_cards(players_hand)
     display_total(players_hand)
-    puts "Hit or Stay? ('h' or 's'):"
-    choice = gets.strip
-    choice == 'h' ? deal_card(deck, players_hand) : break
-    show_cards(players_hand, dealers_hand)
-    break if bust?(players_hand) || twenty_one?(players_hand)
+    break if player_finished?(players_hand)
+    hit_or_stay? == 'hit' ? deal_card(deck, players_hand) : break
   end
-  choice
+  system 'clear' or system 'cls'
+  show_one_card(dealers_hand)
+  show_players_cards(players_hand)
+  display_total(players_hand)
 end
 
 def dealers_turn!(deck, players_hand, dealers_hand)
   unless bust?(players_hand) || twenty_one?(players_hand)
     loop do
+      system 'clear' or system 'cls'
+      show_all_cards(dealers_hand)
+      show_players_cards(players_hand)
+      display_total(players_hand)
       break if total_of(dealers_hand) >= 17
       deal_card(deck, dealers_hand)
+      sleep 1.5
     end
   end
-  show_cards(players_hand, dealers_hand)
 end
 
 def total_of(hand)
@@ -130,11 +158,11 @@ end
 
 def display_total(hand)
   if twenty_one?(hand)
-    puts "Twenty One!"
+    puts "\nTwenty One!"
   elsif bust?(hand)
-    puts "Hand total: BUST!"
+    puts "\nHand total: BUST!"
   else
-    puts "Hand total: #{total_of(hand)}"
+    puts "\nHand total: #{total_of(hand)}"
   end
 end
 
@@ -157,23 +185,27 @@ end
 
 def announce_winner(player, dealer)
   if bust?(player) || !bust?(dealer) && (total_of(dealer) > total_of(player))
-    puts "House Wins"
+    puts "House Wins\n"
   elsif twenty_one?(player)
-    puts "You Win!"
+    puts "You Win!\n"
   elsif total_of(player) == total_of(dealer)
-    puts "Push"
+    puts "Push\n"
   else
-    puts "You Win!"
+    puts "You Win!\n"
   end
 end
 
 # GAME PLAY
-players_hand = []
-dealers_hand = []
-deck = DECK
-deck.shuffle!
-initial_deal(deck, players_hand, dealers_hand)
-show_cards(players_hand, dealers_hand)
-players_turn!(deck, players_hand, dealers_hand)
-dealers_turn!(deck, players_hand, dealers_hand)
-announce_winner(players_hand, dealers_hand)
+loop do
+  players_hand = []
+  dealers_hand = []
+  deck = DECK
+  deck.shuffle!
+  initial_deal(deck, players_hand, dealers_hand)
+  players_turn!(deck, players_hand, dealers_hand)
+  dealers_turn!(deck, players_hand, dealers_hand)
+  announce_winner(players_hand, dealers_hand)
+  puts "Play Again?"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
