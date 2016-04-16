7350d240263ddb461f9020065f73c581ac255097
codeByScott <scott@codebyscott.io>
2016-04-16 16:51:53 +0900

    exercise complete

---
+++ programming-backend-dev/oop/lesson2/modules/pets.rb
@@ -0,0 +1,56 @@
+module Swimmable
+  def swim
+    'Swimming!'
+  end
+end
+
+class Person
+  attr_accessor :name, :pets
+  
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+end
+
+class Pet
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def jump
+    'Jumping!'
+  end
+end
+
+class Cat < Pet
+end
+
+class Dog < Pet
+  include Swimmable
+end
+
+class Bulldog < Dog
+end
+
+bob = Person.new('Bob')
+
+kitty = Cat.new('Kitty')
+tucker = Cat.new('Tucker')
+shadow = Dog.new('Shadow')
+bud = Bulldog.new('Bud')
+
+bob.pets << kitty
+bob.pets << tucker
+bob.pets << shadow
+bob.pets << bud
+
+bob.pets.each do |pet|
+  puts "#{pet.name} is #{pet.jump}"
+end
+
+puts "#{shadow.name} is #{shadow.swim}"
+
